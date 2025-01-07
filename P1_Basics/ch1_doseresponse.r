f8b1d1c52457ba55631e85d9df54cdf9c67744d4
Assaf Oron <assaf.oron@gmail.com>
2025-01-06 22:34:45 -0800

    Ch1 figures

--- P1_Basics/ch1_doseresponse.r
+++ P1_Basics/ch1_doseresponse.r
@@ -1,8 +1,9 @@
 source('basics_header.r')
 
+pdf(file.path(outdir, 'ch1_doseresponse.pdf'), width = 10, height = 7)
 par(mar = c(4,4,1,1), mgp = c(2.5, 0.6, 0), tck = -0.01)
 
-expandedX = (-3):15
+expandedX = seq(-3, 15, 0.1)
 
 plot(expandedX, plogis(expandedX, location = exampleMu, scale = exampleSig), type = 'n', xaxt = 'n', 
 				las = 1, xlab = 'x', ylab = 'Pr (Y = 1)', cex.lab = 1.7, cex.axis = 1.2, bty = 'l' )
@@ -21,4 +22,6 @@ points(exampleMu, 0.5, pch = cchar, cex = csize, col = 'white', bg = 'gray70')
 
 lines(expandedX, plogis(expandedX, location = exampleMu, scale = exampleSig), lwd = 3)
 
-text(-0.8, 0.09, label = 'F(x)', cex = 1.8)
+text(-0., 0.09, label = 'F(x)', cex = 1.5, srt = 15)
+
+dev.off()
