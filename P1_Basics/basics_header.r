57afa2332b803dcd3c19cb8177875acfb66edb61
Assaf Oron <assaf.oron@gmail.com>
2025-03-24 22:17:00 -0700

    ran the 5k :)

--- P1_Basics/basics_header.r
+++ P1_Basics/basics_header.r
@@ -6,8 +6,13 @@ library(upndown)
 exampleMu = 5.6
 exampleSig = 2
 M = 10
+
+# Logistic example used for most figures
 exampleF = plogis(1:M, location = exampleMu, scale = exampleSig)
+# Exponential "counter-example" :)
 exp11F = pexp(1:M, rate=1/11)
 
 outdir = '../../output'
+
+
 
--- P1_Basics/ch3_classic_visits.r
+++ P1_Basics/ch3_classic_visits.r
@@ -23,6 +23,17 @@ f2ij = initvec %*% Wij
 
 fsd = sqrt(f2ij - fij^2)
 
+## Distribution of visits
+
+classic5k = readRDS(file.path(outdir, 'ch3_sim_firstvisit_distribution_classical.rds') ) 
+
+firstvisit = function(x, m) min(which(x == m))
+
+firsts = matrix(NA, nrow = M, ncol = ncol(classic5k$doses) )
+
+for(m in 1:M) firsts[m,] = apply(classic5k$doses[-1, ], 2, firstvisit, m=m)
+
+
 
 #firstimes = ( diag(Z) - Z[startdose, ] ) / cpi
 #firstsq =  ( 2 * diag(Z) - cpi ) / (cpi^2)
